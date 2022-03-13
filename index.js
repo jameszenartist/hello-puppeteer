a0263f3a3ac73c3bc04ddbdf0ad327e522cdca7a
James Hansen <JHansen@NPS.loc>
2022-03-12 22:58:00 -0800

    updated async function

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const puppeteer = require("puppeteer");
 
 const HELLO_WORLD = "https://en.wikipedia.org/wiki/%22Hello,_World!%22_program";
 
-(async function () {
+const WEBSITE_LINK = `${HELLO_WORLD}`;
+
+async function grabScreenshot() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto(HELLO_WORLD);
+  await page.goto(WEBSITE_LINK);
   await page.screenshot({ path: "./screenshot.png" });
   browser.close();
-})();
+}
+
+grabScreenshot();
