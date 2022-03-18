5f41ee7df43534e9250c8427adafe857ba8f093f
James Hansen <jameszenartist@gamil.com>
2022-03-17 21:28:06 -0700

    practiced with imported class instance with testing

---
+++ test/test-2.js
@@ -0,0 +1,47 @@
+const puppeteer = require("puppeteer");
+const expect = require("chai").expect;
+const should = require("chai").should();
+const HomePageTestModel = require("./PageObjectModel/HomePageTestModel.js");
+
+describe("Home page header", () => {
+  let browser;
+  let page;
+  let homePageTestModel;
+
+  before(async () => {
+    browser = await puppeteer.launch();
+  });
+
+  // beforeEach(async () => {
+  //   page = await browser.newPage();
+  //   await page.goto("https://www.packtpub.com/");
+  // });
+
+  //CREATED INSTANCE:
+  beforeEach(async () => {
+    page = await browser.newPage();
+    homePageTestModel = new HomePageTestModel(page);
+    await homePageTestModel.go();
+  });
+
+  afterEach(async () => {
+    await page.close();
+  });
+
+  after(async () => {
+    await browser.close();
+  });
+
+  it("Title should have Packt name", async () => {
+    // const title = await page.title();
+
+    // Replaced page w/ HomePageTestModel class instance:
+
+    const title = await homePageTestModel.title();
+    title.should.contain("Packt");
+  });
+
+  it("Title should mention Books", async () => {
+    expect(await page.title()).to.contain("Books");
+  });
+});
